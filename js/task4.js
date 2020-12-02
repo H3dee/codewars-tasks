c0f9ca11a64b5aaaa018052efc07d8cd449080b3
H3dee <62655938+H3dee@users.noreply.github.com>
2020-12-02 23:51:16 +0300

    Fixed

--- js/task4.js
+++ js/task4.js
@@ -7,17 +7,14 @@ const accum = (sequence) => {
   const separatedSeq = sequence.toLowerCase().split("");
   let accumulator = "";
 
-  separatedSeq.forEach((char, index) => {
+  separatedSeq.forEach((char, index, arr) => {
     const subSeq = Array.from({ length: index + 1 }, (_, i) => {
-      return (
-        (i === 0 ? char.toUpperCase() : char) + (i === index + 1 ? "" : "-")
-      );
+      return i === 0 ? char.toUpperCase() : char;
     });
+    index + 1 !== arr.length && subSeq.push("-");
 
-    accumulator += subSeq;
+    accumulator += subSeq.join("");
   });
 
   return accumulator;
 };
-
-
