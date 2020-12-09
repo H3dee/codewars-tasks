32388043aa4fdff6b9c9647d358e1c69992d0839
H3dee <62655938+H3dee@users.noreply.github.com>
2020-12-09 15:14:55 +0300

    Fixed by reduce

--- js/task8.js
+++ js/task8.js
@@ -1,20 +1,11 @@
 /**
  * Complete the solution so that the function will break up camel casing, using a space between words.
-
 Example
 solution("camelCasing")  ==  "camel Casing"
  */
 
-const solution = (str) => {
-  if(str && typeof str === 'string') {
-    return str
-    .split("")
-    .map(
-      (char, index, arr) =>
-        char.toUpperCase() === char && arr.splice(index, 0, " ")
-    )
-    .join(" ");
-  }
-
-  return ''
-};
+const solution = (str) =>
+  str.split('').reduce((acc, char) => {
+    if (char === char.toUpperCase()) return (acc += ` ${char}`)
+    else return acc += char
+  }, '')
